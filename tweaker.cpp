7f4a942a6c35b2dc1639fd5862d9e262b00b07b0
TigerHuang <ynotstartups@gmail.com>
2018-03-03 16:41:33 +0000

    tweak to first biggest area vector save to stl

--- tweaker.cpp
+++ tweaker.cpp
@@ -4,7 +4,11 @@
 
 #include <vcg/complex/complex.h>
 #include <wrap/io_trimesh/import_stl.h>
+#include <wrap/io_trimesh/export_stl.h>
 #include <vcg/complex/algorithms/clean.h>
+#include <vcg/complex/algorithms/update/position.h>
+
+#include <Eigen/Geometry>
 
 class MyVertex; class MyFace;
 struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>   ::AsVertexType,
@@ -72,7 +76,7 @@ int main( int argc, char *argv[] )
         return 1;
     }
 
-    vcg::tri::UpdateNormal<MyMesh>::PerFace(mesh);
+    vcg::tri::UpdateNormal<MyMesh>::PerFaceNormalized(mesh);
 
     for (auto fi = mesh.face.begin(); fi!=mesh.face.end(); ++fi) {
         // printf("normal 0 %f ", (*fi).N()[0]);
@@ -83,9 +87,49 @@ int main( int argc, char *argv[] )
         counter[test]+=doubleArea;
     }
 
-    for ( auto it = counter.begin(); it != counter.end(); ++it ) {
-        if (it->second > 1)
-            std::cout << ":" << it->second;
+    std::vector<std::pair<std::array<float, 3>, float>> top_normal(10);
+    std::partial_sort_copy(counter.begin(), counter.end(),
+            top_normal.begin(),
+            top_normal.end(),
+            [](std::pair<std::array<float, 3>, float> const& l,
+               std::pair<std::array<float, 3>, float> const& r)
+            {
+            return l.second >= r.second;
+            }
+    );
+
+    for ( auto it = top_normal.begin(); it != top_normal.end(); ++it ) {
+            printf("normal %f %f %f %f \n",it->first[0], it->first[1], it->first[2], it->second);
     }
+
+    // vcg::tri::UpdatePosition<MyMesh>::Matrix(mesh, )
+
+    const Eigen::Vector3f to = {0, 0, -1};
+    const Eigen::Vector3f from = {top_normal[0].first[0], top_normal[0].first[1], top_normal[0].first[2]};
+
+    Eigen::Matrix3f r = Eigen::Quaternionf().setFromTwoVectors(from,to).toRotationMatrix();
+    Eigen::Matrix4f mat4 = Eigen::Matrix4f::Identity(); mat4.block(0,0,3,3) = r;
+
+    vcg::Matrix44<float> transformation_matrix;
+    transformation_matrix.FromEigenMatrix(mat4);
+
+    int count = 0;
+    for (auto vi = mesh.vert.begin(); vi!=mesh.vert.end(); ++vi) {
+        count++;
+        if (count == 10) break;
+        printf("before v %f %f %f\n", vi->cP()[0], vi->cP()[1], vi->cP()[2]);
+    }
+    vcg::tri::UpdatePosition<MyMesh>::Matrix(mesh, transformation_matrix);
+
+    std::cout << r << std::endl;
+
+    count = 0;
+    for (auto vi = mesh.vert.begin(); vi!=mesh.vert.end(); ++vi) {
+        count++;
+        if (count == 10) break;
+        printf("after v %f %f %f\n", vi->cP()[0], vi->cP()[1], vi->cP()[2]);
+    }
+
+    vcg::tri::io::ExporterSTL<MyMesh>::Save(mesh, "./out/tweaked.stl");
     return 0;
 }
